a4a622d3c6c67995d19f68cebe51a392b05b23c2
Remark Timones <202180037@psu.palawan.edu.ph>
2023-12-11 15:13:28 +0800

    new

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
 
 $conn = mysqli_connect('localhost','root','','Web');
-
+if ($conn->connect_error) 
+        die("Connection failed: " . $conn->connect_error);
 ?>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Landing Page</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous" />
+
+    <style>
+      body {
+        background: linear-gradient(
+          180deg,
+          #5e62a1 10%,
+          #8276a5 25%,
+          #997fa7 35%,
+          #b386a7 45%,
+          #d288a5 60%,
+          #f9a08f 80%,
+          #feb573 100%
+        );
+      }
+      main {
+        max-width: 100%;
+        margin-top: -8px;
+        height: 92.9vh;
+      }
+
+      header {
+        border-bottom: 1px solid;
+      }
+
+      #form div a {
+        background-color: #826599;
+        color: white;
+      }
+
+      #form div a:hover {
+        background-color: #833e6b;
+      }
+    </style>
+  </head>
+  <body>
+    <header
+      class="container-fluid justify-content-center d-flex p-3 mb-2"
+      id="form">
+      <div>
+        <a class="btn mx-5" href="login_page.php">Log In</a>
+      </div>
+
+      <div>
+        <a class="btn mx-5" href="registration_page.php">Sign Up</a>
+      </div>
+    </header>
+
+    <main>
+      <section></section>
+    </main>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"></script>
+  </body>
+</html>
